ff0b2cddbbf6d645d5f88e3610f223dee65f5e3d
alvdeveloper <alv@alvsoftwarellc.com>
2016-06-28 14:04:51 -0400

    Revert "fixing basic social ui"

--- server/config/seed.js
+++ server/config/seed.js
@@ -61,16 +61,7 @@ var Deal = sqldb.Deal;
           password: 'Baiser12!',
           role: 'admin',
           provider: 'local',
-        },
-        {
-          firstName: 'Cary',
-          lastName: 'Gaskell',
-          email: 'carylgaskell@gmail.com',
-          phone: '5713289692',
-          password: 'Baiser12!',
-          role: 'admin',
-          provider: 'local',
-        },{
+        }, {
           firstName: 'Miles',
           lastName: 'Johnson',
           email: 'mjohnson@alvsoftwarellc.com',
