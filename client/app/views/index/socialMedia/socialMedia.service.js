30c3fc1c6e04e8923dc18640b3b0fb9afb1b8d51
Ludovic Agodio <lagodio@alvsoftwarell.com>
2016-06-29 00:19:30 -0400

    Social Data

---
+++ client/app/views/index/socialMedia/socialMedia.service.js
@@ -0,0 +1,37 @@
+'use strict';
+
+angular.module('dealScanCrmApp')
+  .factory('SocialMedia', function (Auth, Util, ezfb, $q) {
+    // Service logic
+    var _socialSearchResults = { data: [], params: {}};
+
+
+
+    //Search facebook
+    function fbSearch(term){
+      
+
+    }
+
+
+    //Search Twitter
+    function twtSearch(term){
+
+
+
+    }
+
+
+    //search Instagram
+    function igSearch(term){
+
+
+
+    }
+
+    // Public API here
+    return {
+
+    };
+
+  });
