2fd80f19f87f84bb2f16f3d34492d7e1bf3caee5
carygaskell <carylee.gaskell@thehumangeo.com>
2016-06-28 22:51:13 -0400

    add map height directive

--- client/app/app.directives.js
+++ client/app/app.directives.js
@@ -501,6 +501,17 @@ function landingScrollspy(){
   }
 }
 
+var addEvent = function(object, type, callback) {
+  if (object == null || typeof(object) == 'undefined') return;
+  if (object.addEventListener) {
+    object.addEventListener(type, callback, false);
+  } else if (object.attachEvent) {
+    object.attachEvent("on" + type, callback);
+  } else {
+    object["on"+type] = callback;
+  }
+};
+
 /**
  * fitHeight - Directive for set height fit to window height
  */
@@ -510,6 +521,16 @@ function fitHeight(){
     link: function(scope, element) {
       element.css("height", $(window).height() + "px");
       element.css("min-height", $(window).height() + "px");
+    }
+  };
+}
+
+function fitMap(){
+  return {
+    restrict: 'A',
+    link: function(scope, element) {
+      element.css("height", $(window).height() - 157 + "px");
+      element.css("min-height", $(window).height() - 157 + "px");
     }
   };
 }
@@ -526,6 +547,11 @@ function pageHeight(){
         console.log("setting height for social media page...");
         element.css("height", $(window).height() + "px");
         element.css("min-height", $(window).height() + "px");
+        element.css("overflow", "hidden");
+        addEvent(window, "resize", function(event) {
+          element.css("height", $(window).height() + "px");
+          element.css("min-height", $(window).height() + "px");
+        });
       }
     }
   };
@@ -612,6 +638,7 @@ angular
   .directive('landingScrollspy', landingScrollspy)
   .directive('fitHeight', fitHeight)
   .directive('pageHeight', pageHeight)
+  .directive('fitMap', fitMap)
   .directive('iboxToolsFullScreen', iboxToolsFullScreen)
   .directive('slimScroll', slimScroll)
   .directive('truncate', truncate)
