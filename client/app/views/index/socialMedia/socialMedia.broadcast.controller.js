20a7e3b032f2d35e23edcfc7aff9c4dcf26a3a80
carygaskell <carylee.gaskell@thehumangeo.com>
2016-07-05 07:38:18 -0400

    tweet function

--- client/app/views/index/socialMedia/socialMedia.broadcast.controller.js
+++ client/app/views/index/socialMedia/socialMedia.broadcast.controller.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('dealScanCrmApp')
-  .controller('SocialMediaBroadcastCtrl', function ($scope, Auth, Util, $filter, $uibModalInstance) {
+  .controller('SocialMediaBroadcastCtrl', function ($scope, Auth, Util, $filter, $uibModalInstance, SocialMedia) {
 
     var _smBroadcast =this;
     _smBroadcast.user = Auth.getCurrentUser();
@@ -15,10 +15,24 @@ angular.module('dealScanCrmApp')
     };
 
     _smBroadcast.broadcastObj = {text: '', fb: false, twt: true};
-    
+
+    /**
+     * Twitter status update
+     * @param message
+     */
+    _smBroadcast.tweet = function(){
+      if (!_smBroadcast.broadcastObj.text.length < 1) return;
+      SocialMedia.tweet(_smBroadcast.broadcastObj.text).then(function(res){
+        if (res && res.success){
+          toaster.success({title:'Twitter Status Update', body: res.message});
+          _smBroadcast.ok();
+        } else toaster.error({title: 'Twitter Status Update', body: 'Unable to post status update'});
+      }).catch(function(err){
+        console.log(err);
+        toaster.error({title: 'Twiiter Status Update', body: err});
+      });
+    }
     
 
   });
-/**
- * Created by ludovicagodio on 6/18/16.
- */
+
