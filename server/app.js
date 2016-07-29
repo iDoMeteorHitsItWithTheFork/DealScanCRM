a1b2ff4b9508f86ccafb502943b9284f1ae1cb54
Ludovic Agodio <lagodio@alvsoftwarell.com>
2016-07-29 13:13:46 -0400

    Remove Action Hero as API Server....

--- server/app.js
+++ server/app.js
@@ -38,6 +38,14 @@ function startServer() {
   app.angularFullstack = server.listen(config.port, config.ip, function() {
     console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
     //require('./backgroundTasks/DbSync').start();
+    //google authentication.....
+
+
+
+
+
+
+
   });
 }
 
